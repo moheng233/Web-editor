a908e2a136ca7ceb39c545de19bbe87c06d0c635
ylm200322400 <ylm200322400>
2019-11-15 09:30:17 +0800

    基础组件库增加普通文字组件

--- wed_component.js
+++ wed_component.js
@@ -14,9 +14,9 @@ $(function(){
                     <add></div>\
                 </div>\
             </div>',
-        edit:[
-            {
-                eid: 'card_header_text',
+        edit:{
+            card_header_text: {
+                eid: '',
                 name: '卡片头部标题',
                 type: 'text',
                 get: function(component){
@@ -26,7 +26,7 @@ $(function(){
                     return component.find('.card-header').html(value);
                 }
             }
-        ],
+        },
         into: function(){
             return wed.editor.insert(this);
         }
@@ -62,10 +62,9 @@ $(function(){
     wed.register.component('text',{
         cid: 'h1',
         name: '一级标题',
-        template: '<h1 edit>一级标题</h1>',
-        edit: [
-            {
-                eid: 'text',
+        template: '<p class="h1" edit>一级标题</p>',
+        edit: {
+            text: {
                 name: '文字',
                 type: 'text',
                 get: function(component){
@@ -74,8 +73,56 @@ $(function(){
                 set: function(component,value){
                     return component.html(value);
                 }
+            },
+            h: {
+                name: '标题类型',
+                type: 'select',
+                select: {
+                    h1: '一级标题',
+                    h2: '二级标题',
+                    h3: '三级标题',
+                    h4: '四级标题',
+                    h5: '五级标题'
+                },
+                get: function(component){
+                    return component.attr('class');
+                },
+                set: function(component,value){
+                    component.removeClass();
+                    return component.addClass(value);
+                }
             }
-        ],
+        },
+        into: function(){
+            return wed.editor.insert(this);
+        }
+    })
+    wed.register.component('text',{
+        cid: 'p',
+        name: '文字',
+        template: '<p edit>普通文字</p>',
+        edit: {
+            text: {
+                name: '文字',
+                type: 'text',
+                get: function(component){
+                    return $.trim(component.html());
+                },
+                set: function(component,value){
+                    return component.html(value);
+                }
+            },
+            size: {
+                name: '文字大小',
+                type: 'text',
+                get: function(component){
+                    return component.css('font-size');
+                },
+                set: function(component,value){
+                    return component.css('font-size',value);
+                }
+            }
+        },
         into: function(){
             return wed.editor.insert(this);
         }
